e4cdd743050d42b271bc4f8fde84803d75ad2f6a
Amoki <hugo@bimdata.io>
2026-04-25 00:03:08 +0200

    fix pint power icon

--- CharolaisCode/Powers/CharolaisPower.cs
+++ CharolaisCode/Powers/CharolaisPower.cs
@@ -2,12 +2,22 @@
 using BaseLib.Extensions;
 using Charolais.CharolaisCode.Extensions;
 using Godot;
+using MegaCrit.Sts2.Core.Logging;
 
 namespace Charolais.CharolaisCode.Powers;
 
 public abstract class CharolaisPower : CustomPowerModel
 {
     //Loads from Charolais/images/powers/your_power.png
-    public override string CustomPackedIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".PowerImagePath();
+    public override string CustomPackedIconPath
+    {
+        get
+        {
+            Log.Warn("path to pint power image");
+            Log.Warn($"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".PowerImagePath());
+            return $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".PowerImagePath();
+        }
+    }
+
     public override string CustomBigIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigPowerImagePath();
 }
--- CharolaisCode/Powers/PintPower.cs
+++ CharolaisCode/Powers/PintPower.cs
@@ -1,14 +1,12 @@
-﻿using MegaCrit.Sts2.Core.Entities.Creatures;
-using MegaCrit.Sts2.Core.Entities.Powers;
+﻿using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
-using BaseLib.Abstracts;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 
 namespace Charolais.CharolaisCode.Powers;
 
-public class PintPower : CustomPowerModel
+public class PintPower : CharolaisPower
 {
     public override PowerType Type => PowerType.Buff;
 
@@ -19,14 +17,8 @@ public class PintPower : CustomPowerModel
     public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
     {
         if (side != base.Owner.Side || base.Owner?.Player?.PlayerCombatState == null) return;
-        try
-        {
-            Flash();
-            await CreatureCmd.GainBlock(base.Owner, base.Amount, ValueProp.Unpowered, null);
-        }
-        catch (Exception ex)
-        {
-            Godot.GD.PrintErr($"[{GetType().Name}] Error in afterTurnEnd: " + ex);
-        }
+
+        Flash();
+        await CreatureCmd.GainBlock(base.Owner, base.Amount, ValueProp.Unpowered, null);
     }
 }
