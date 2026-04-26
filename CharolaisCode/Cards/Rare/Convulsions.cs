54727666a243309d2b395562830928f20b229fce
SweetSkippy <bastien.pellat@wanadoo.fr>
2026-04-26 21:18:01 +0200

    new cards

---
+++ CharolaisCode/Cards/Rare/Convulsions.cs
@@ -0,0 +1,57 @@
+﻿using Charolais.CharolaisCode.Cards;
+using Charolais.CharolaisCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Charolais.CharolaisCode.Cards.Common;
+
+
+
+public class Machine() : CharolaisCard(1, CardType.Attack, CardRarity.Basic, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(15, ValueProp.Move),
+        new CalculationBaseVar(0m),
+        new ExtraDamageVar(1m),
+        new CalculatedDamageVar(ValueProp.Move).WithMultiplier((CardModel card, Creature? target) => card.Owner.Creature.GetPowerAmount<PintPower>())
+    ];
+    
+    public override CardMultiplayerConstraint MultiplayerConstraint => CardMultiplayerConstraint.MultiplayerOnly;
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+        await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).FromCard(this).Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+        
+        var players = base.CombatState?.Players;
+        if (players == null) { return; }
+        var alcoolPower = base.Owner.Creature.GetPowerAmount<PintPower>();
+        if (alcoolPower > 0)
+        {
+            int index = new Random().Next(players.Count);
+            Player randomPlayer = players[index];
+            await Cmd.Wait(0.5f);
+            await DamageCmd.Attack(alcoolPower).FromCard(this).Targeting(randomPlayer.Creature)
+                .WithHitFx("vfx/vfx_attack_slash")
+                .Execute(choiceContext);
+        }
+        
+        
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars.Damage.UpgradeValueBy(3m);
+    }
+}
