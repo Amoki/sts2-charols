14d046c9d2bd7e6b8415dd72af6b07154f2a121a
SweetSkippy <bastien.pellat@wanadoo.fr>
2026-04-28 17:46:32 +0200

    add bite-signal

---
+++ CharolaisCode/Cards/Uncommon/bitesignal.cs
@@ -0,0 +1,26 @@
+﻿using Charolais.CharolaisCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Charolais.CharolaisCode.Cards.Uncommon;
+
+public class Bitesignal() : CharolaisCard(1,
+    CardType.Power, CardRarity.Uncommon,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [(DynamicVar) new BlockVar(3m, ValueProp.Move)];
+    
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PowerCmd.Apply<BitesignalPower>(choiceContext, this.Owner.Creature, this.DynamicVars.Block.BaseValue, this.Owner.Creature,this);
+    }
+    
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars.Block.UpgradeValueBy(1);
+        this.AddKeyword(CardKeyword.Innate);
+    }
+}
---
+++ CharolaisCode/Powers/BitesignalPower.cs
@@ -0,0 +1,21 @@
+﻿using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Charolais.CharolaisCode.Powers;
+
+public class BitesignalPower : CharolaisPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+    
+    public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+    {
+        if (side != base.Owner.Side || base.Owner?.Player?.PlayerCombatState == null) return;
+        
+        Flash();
+        await CreatureCmd.GainBlock(base.Owner, base.Amount, ValueProp.Unpowered, null);
+    }
+}
