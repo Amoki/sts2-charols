3cce006d5de14a6bacd6e1fb3bf0091f01b4f6ad
Amoki <hugo@bimdata.io>
2026-05-07 22:02:25 +0200

    fix muraille de boules

--- CharolaisCode/Cards/Rare/Murailledeboules.cs
+++ CharolaisCode/Cards/Rare/Murailledeboules.cs
@@ -1,8 +1,14 @@
-﻿using MegaCrit.Sts2.Core.Commands;
+﻿using System.Globalization;
+using BaseLib.Cards.Variables;
+using BaseLib.Extensions;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
 
@@ -15,24 +21,41 @@ public class Murailledeboules() : CharolaisCard(1, CardType.Skill, CardRarity.Ra
     protected override HashSet<CardTag> CanonicalTags => [PetanqueTag.Petanque];
     
     protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new BlockVar(8m, ValueProp.Move)
+        new CalculationBaseVar(8m),
+        new CalculationExtraVar(1m),
+        new CalculatedBlockVar(ValueProp.Unpowered).WithMultiplier((card, _) => GetOwnerDextAmount(card) * 3)
     ];
     
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [(HoverTipFactory.FromPower<DexterityPower>())];
-
-    // ToDO fix calculated block in description in fight
     
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        var dextPower = base.Owner.Creature.GetPowerAmount<DexterityPower>();
-        var doubleDextPower = dextPower * 2;
-        await PowerCmd.Apply<DexterityPower>(choiceContext, this.Owner.Creature, doubleDextPower, this.Owner.Creature, this);
-        await CreatureCmd.GainBlock(base.Owner.Creature, base.DynamicVars.Block, cardPlay);
-        await PowerCmd.Apply<DexterityPower>(choiceContext, this.Owner.Creature, decimal.Negate(doubleDextPower), this.Owner.Creature,  this);
+        await CreatureCmd.GainBlock(base.Owner.Creature, base.DynamicVars.CalculatedBlock.Calculate(cardPlay.Target), base.DynamicVars.CalculatedBlock.Props, cardPlay);
     }
 
     protected override void OnUpgrade()
     {
-        this.DynamicVars.Block.UpgradeValueBy(4M);
+        this.DynamicVars.CalculationBase.UpgradeValueBy(4M);
+    }
+    
+    private static decimal GetOwnerDextAmount(CardModel card)
+    {
+        if (!card.IsMutable)
+        {
+            return 0m;
+        }
+        if (card.Owner == null)
+        {
+            return 0m;
+        }
+        if (card.Pile == null)
+        {
+            return 0m;
+        }
+        if (!card.Pile.IsCombatPile)
+        {
+            return 0m;
+        }
+        return card.Owner.Creature.GetPowerAmount<DexterityPower>();
     }
 }
