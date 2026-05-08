74337ab00018a7128c5eafe199d2bbc9b1bddf4d
Amoki <hugo@bimdata.io>
2026-05-08 19:09:55 +0200

    fix ouvertune italienne no draw if target dies, fix forme du charolais description missmatch

--- CharolaisCode/Cards/Common/Louvertureitalienne.cs
+++ CharolaisCode/Cards/Common/Louvertureitalienne.cs
@@ -26,12 +26,11 @@ public class Louvertureitalienne() : CharolaisCard(1,
     {
         if (cardPlay.Target != null)
         {
+            var target = cardPlay.Target;
+            var amount = target.GetPowerAmount<ChestPower>();
             await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(cardPlay.Target)
                 .WithHitFx("vfx/vfx_starry_impact", "blunt_attack.mp3")
                 .Execute(choiceContext);
-            var target = cardPlay.Target;
-            if (target == null || !target.IsAlive) return;
-            var amount = target.GetPowerAmount<ChestPower>();
             if (amount > 0)
             {
                 await CardPileCmd.Draw(choiceContext, this.DynamicVars.Cards.BaseValue, this.Owner);
