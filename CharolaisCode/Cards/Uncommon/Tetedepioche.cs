25f7c9fe1c2de03371123db68b0637c23a35191a
SweetSkippy <bastien.pellat@wanadoo.fr>
2026-05-13 20:08:09 +0200

    fix tetedepioche add potiondepetanque image potiondeflotte

--- CharolaisCode/Cards/Uncommon/Tetedepioche.cs
+++ CharolaisCode/Cards/Uncommon/Tetedepioche.cs
@@ -1,9 +1,11 @@
 ﻿using Charolais.CharolaisCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Charolais.CharolaisCode.Cards.Uncommon;
@@ -29,9 +31,12 @@ public class Tetedepioche() : CharolaisCard(2, CardType.Skill, CardRarity.Uncomm
         CardPlay cardPlay)
     {
         ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
-        await DamageCmd.Attack(this.DynamicVars.CalculatedDamage).FromCard(this).Targeting(cardPlay.Target)
-                .WithHitFx("vfx/vfx_attack_slash")
-                .Execute(choiceContext);
+
+        if (this.DynamicVars.ContainsKey("CalculatedDamage"))
+        {
+            DamageVar damage = new DamageVar(this.DynamicVars.CalculatedDamage.Calculate(null), ValueProp.Unblockable | ValueProp.Unpowered | ValueProp.Move);
+            await CreatureCmd.Damage(choiceContext, cardPlay.Target, damage, (CardModel)this);
+        }
     }
     
     protected override void OnUpgrade()
---
+++ CharolaisCode/Potions/Potiondepetanque.cs
@@ -0,0 +1,64 @@
+﻿using Charolais.CharolaisCode.Cards;
+using Charolais.CharolaisCode.Character;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Potions;
+using MegaCrit.Sts2.Core.Factories;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+
+namespace Charolais.CharolaisCode.Potions;
+
+public class Potiondepetanque : CharolaisPotion
+{
+    public override PotionRarity Rarity => PotionRarity.Rare;
+
+    public override PotionUsage Usage => PotionUsage.CombatOnly;
+
+    public override TargetType TargetType => TargetType.Self;
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DynamicVar("Power", 1)
+    ];
+
+    public override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromPower<StrengthPower>(),
+        HoverTipFactory.FromPower<DexterityPower>()
+    ];
+    
+    protected override async Task OnUse(PlayerChoiceContext choiceContext, Creature? target)
+    {
+        await PowerCmd.Apply<StrengthPower>(choiceContext, target!, this.DynamicVars["Power"].IntValue, this.Owner.Creature, null);
+        await PowerCmd.Apply<DexterityPower>(choiceContext, target!, this.DynamicVars["Power"].IntValue, this.Owner.Creature, null);
+
+        var player = target!.Player;
+        
+        var cardPool = ModelDb.CardPool<CharolaisCardPool>()
+            .GetUnlockedCards(player!.UnlockState, player!.RunState.CardMultiplayerConstraint);
+        
+        var filteredCards = cardPool.Where(c => 
+            c.Tags.Contains(PetanqueTag.Petanque)
+        );
+        
+        var cardsToGenerate = CardFactory.GetDistinctForCombat(
+            player, 
+            filteredCards, 
+            1, 
+            player.RunState.Rng.CombatCardGeneration
+        ).ToList();
+        
+        foreach (var cardModel in cardsToGenerate)
+        {
+            cardModel.SetToFreeThisTurn();
+        }
+        
+        await CardPileCmd.AddGeneratedCardsToCombat(cardsToGenerate, PileType.Hand, player);
+        
+    }
+}
