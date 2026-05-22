4bf5308db4d94ba1634e7f61c3b5f849557e73fe
SweetSkippy <bastien.pellat@wanadoo.fr>
2026-05-22 13:23:36 +0200

    add proxi minorfix burger

--- CharolaisCode/Cards/Common/Proxi.cs
+++ CharolaisCode/Cards/Common/Proxi.cs
@@ -1,15 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using MegaCrit.Sts2.Core.CardSelection;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.Powers;
 
 namespace Charolais.CharolaisCode.Cards.Common;
 
-public class Burger() : CharolaisCard(1,
+public class Proxi() : CharolaisCard(1,
     CardType.Skill, CardRarity.Common,
     TargetType.Self)
 {
@@ -17,19 +17,25 @@ public class Burger() : CharolaisCard(1,
 
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new DynamicVar("Power",5M)
+        new CardsVar(1),
+        new DynamicVar("Power",2M)
     ];
     
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [(HoverTipFactory.FromPower<VigorPower>())];
     
+    private static LocString GetSelectionPrompt => new LocString("card_selection", "TO_GET");
+    
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await CreatureCmd.TriggerAnim(this.Owner.Creature, "Cast", this.Owner.Character.CastAnimDelay);
         await PowerCmd.Apply<VigorPower>(choiceContext, this.Owner.Creature, this.DynamicVars["Power"].IntValue, this.Owner.Creature, this);
+        int selectCount = Math.Min(this.DynamicVars.Cards.IntValue, CardPile.MaxCardsInHand - PileType.Hand.GetPile(this.Owner).Cards.Count);
+        if (selectCount <= 0)
+            return;
+        await CardPileCmd.Add(await CardSelectCmd.FromSimpleGrid(choiceContext, PileType.Discard.GetPile(this.Owner).Cards, this.Owner, new CardSelectorPrefs(GetSelectionPrompt, selectCount)), PileType.Hand);
     }
     
     protected override void OnUpgrade()
     {
-        this.DynamicVars["Power"].UpgradeValueBy(3M);
+        this.DynamicVars["Power"].UpgradeValueBy(2M);
     }
 }
