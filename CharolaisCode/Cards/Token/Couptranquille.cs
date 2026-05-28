48a2bfff5193b3d77c6439cbafa926dca5804e54
Amoki <hugo@bimdata.io>
2026-05-28 17:39:36 +0200

    implement couptranquille upgrade at the end of turn

--- CharolaisCode/Cards/Token/Couptranquille.cs
+++ CharolaisCode/Cards/Token/Couptranquille.cs
@@ -1,10 +1,14 @@
 ﻿using BaseLib.Utils;
 using Charolais.CharolaisCode.Powers;
+using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Logging;
 using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Charolais.CharolaisCode.Cards.Token;
 
@@ -26,6 +30,22 @@ public class Couptranquille() : CharolaisCard(0, CardType.Skill, CardRarity.Toke
         await PowerCmd.Apply<ChestPower>(choiceContext, cardPlay.Target ?? throw new InvalidOperationException(), DynamicVars["Chest"].IntValue, this.Owner.Creature, this);
         await CardPileCmd.Draw(choiceContext, this.DynamicVars.Cards.BaseValue, this.Owner);
     }
+    
+    public override async Task AfterSideTurnEndLate(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
+    {
+        if (side == CombatSide.Player && this.Pile!.Type == PileType.Hand)
+        {
+            if (this.IsUpgraded)
+            {
+                this.DynamicVars["Chest"].UpgradeValueBy(3M);
+            }
+            else
+            {
+                this.DynamicVars["Chest"].UpgradeValueBy(2M);
+            }
+        }
+    }
+
     
     protected override void OnUpgrade()
     {
